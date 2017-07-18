fcfcbafb78744521b05a0ff586fb4e664a21658e
Katherine Halabi <katherineghalabi@gmail.com>
2017-07-18 17:22:17 -0400

    Create Q06_B.java

---
+++ Q06_B.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Paridade 
+{
+
+    public static void main(String[] args) 
+    {
+        int n;
+        Scanner num = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        N = num.nextInt();
+        if ((n%2) == 0)
+        {
+            System.out.println("O valor "+N+" é par.\n");
+        } 
+        else 
+        {
+            System.out.println("O valor "+N+" é ímpar.\n");
+        }
+     }
+}
