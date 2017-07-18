20814bbfdac5e3fc6367613d88cd597107b33e67
Katherine Halabi <katherineghalabi@gmail.com>
2017-07-18 16:12:45 -0400

    Update Q06_A.java

--- Q06_A.java
+++ Q06_A.java
@@ -15,20 +15,20 @@ import java.util.Scanner;
 	    media = (n1 + n2 + n3 + n4)/4;
 
 	    if (media >= 7)
-    {
+            {
 	        System.out.println("Aluno Aprovado\n" + media);
 	    }
-    else 
-    {
+   	    else 
+            {
 	        System.out.println("Nota de Exame Final: ");
 	        ex = nota.nextFloat();
 	        nova_media = (media + ex)/2;
 	        if(nova_media >= 5)
-        {
+                {
 	            System.out.println("Aluno Aprovado em Exame " + nova_media);
 	        } 
-        else 
-        {
+                else 
+                {
 	            System.out.println("Aluno Reprovado " + media2);
 	        }
 	    }
