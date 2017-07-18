39f1d3f0680fb474fed64cdec1ebfad47757014f
Katherine Halabi <katherineghalabi@gmail.com>
2017-07-18 16:13:21 -0400

    Update Q06_A.java

--- Q06_A.java
+++ Q06_A.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 	
 	public class main 
-{
+        {
 	    public static void main(String[] args)
-    {
+            {
     Scanner nota = new Scanner(System.in);
 	    double n1, n2, n3, n4, media, ex, nova_media;
 	    
